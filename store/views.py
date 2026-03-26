90795c2ffa0c3940979239a0f43f86f6bf812699
Nguyên Tịnh <tinbo515@outlook.com>
2026-03-26 16:46:51 +0700

    Update views.py

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
 from django.db.models import Q
 from .models import Product, Cart, Message, Profile
+from .models import Notification
 
 
 # =====================
