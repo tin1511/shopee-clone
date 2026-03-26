d4ee1ffa5b1756240f6494ea13de1b08845a7cb3
Nguyên Tịnh <tinbo515@outlook.com>
2026-03-26 15:00:28 +0700

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('notifications/', views.notifications),
     
     path('checkout/', views.checkout),
     
